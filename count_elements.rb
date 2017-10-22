6404600d9e34a7a508d9e55740e21bc3c6614734
Dakota Lillie <dakota.lillie@icloud.com>
2017-10-22 16:25:55 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,9 @@
+require 'pry'
+
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  array.each {|element| new_hash[element] == nil ? new_hash[element] = 1 : new_hash[element] += 1}
+  new_hash
 end
  
